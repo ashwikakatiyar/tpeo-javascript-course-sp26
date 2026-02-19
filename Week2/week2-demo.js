ad819cc5eb819a692e57764b1004e397930bf48e
Ashwika Katiyar <ashwikakatiyar@gmail.com>
2026-02-19 16:24:00 -0600

    Week 2

--- Week2/week2-demo.js
+++ Week2/week2-demo.js
@@ -4,21 +4,11 @@
 
 // Write your function here
 function calculateTriangleArea(base, height) {
-    return (base * height) / 2;
-    // console.log(base);   <- unreachable code! 
+    // TODO
 } 
 
-// console.log(base);  <- cannot access parameters outside of function! 
-
 // Define some variables to test it here
-var b = 5;
-let h = 3;
 
 // Able to reassign
-b = 10;
-h = 6;
 
 // Call your function here!
-var area;
-area = calculateTriangleArea(b, h);
-console.log(area);      // should print 30.
