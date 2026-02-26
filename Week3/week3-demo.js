21d8c24b7d5d71e32a310a1c444b925845f12b11
Ashwika Katiyar <ashwikakatiyar@gmail.com>
2026-02-26 17:14:12 -0600

    Pushing Week 2 changes + Week 3 prep

---
+++ Week3/week3-demo.js
@@ -0,0 +1,15 @@
+// WEEK 3 DEMO: 
+/*
+Task: write a function, walkToTreasure, that takes in x and y coordinates on a beach 
+and “walks” to the location of the treasure buried in the sand. 
+*/
+
+function walkToTreasure(x, y, treasureX, treasureY) {
+
+    // Find x ccordinate
+    
+
+    // Find y coordinate
+
+  
+}
