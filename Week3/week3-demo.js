48b0f719d1858aabc53ddc633dbfdb5e73c75b79
Ashwika Katiyar <ashwikakatiyar@gmail.com>
2026-02-26 18:52:16 -0600

    Pushing Week 3 solutions + Week 4 prep
    Added logic to navigate to the treasure coordinates.

--- Week3/week3-demo.js
+++ Week3/week3-demo.js
@@ -7,9 +7,28 @@ and “walks” to the location of the treasure buried in the sand.
 function walkToTreasure(x, y, treasureX, treasureY) {
 
     // Find x ccordinate
-    
+    while (x != treasureX) {
+        if (x < treasureX) {
+            console.log("Going right");
+            x++;
+        } else {
+            console.log("Going left");
+            x--;
+        }
+    }
 
     // Find y coordinate
+    while (y != treasureY) {
+        if (y < treasureY) {
+            console.log("Going forward");
+            y++;
+        } else {
+            console.log("Going backward");
+            y--;
+        }
+    }
 
-  
+    console.log("Found treasure!");
 }
+
+walkToTreasure(0, 0, );
